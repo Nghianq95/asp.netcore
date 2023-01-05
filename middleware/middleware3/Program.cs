d797698e79c59cbc70870d8541139c28c9a673ae
Nguyễn Quang Nghĩa <38548263+Nghianq95@users.noreply.github.com>
2023-01-05 22:32:27 +0900

    add middleware 3

---
+++ middleware/middleware3/Program.cs
@@ -0,0 +1,29 @@
+﻿var app = WebApplication.Create();
+
+app.Use(BufferAsync);
+app.Run(async (context)=>{
+    context.Response.Headers.Add("Content-Type", "text/html");
+    await context.Response.WriteAsync("<h1>This sample uses bufferin</h1>");
+    await context.Response.WriteAsync("<p>This allows all your Middlewares to write to Response</p>");
+});
+app.Run();
+
+async Task BufferAsync(HttpContext context, RequestDelegate next)
+{
+    var body = context.Response.Body;
+    var buffer = new MemoryStream();
+    context.Response.Body = buffer;
+
+    try
+    {
+        await context.Response.WriteAsync("<html><body>");
+        await next(context);
+        await context.Response.WriteAsync("</body></html>");
+
+        buffer.Position = 0;
+        await buffer.CopyToAsync(body);
+    }
+    finally{
+        context.Response.Body = body;
+    }
+}
--- readme.md
+++ readme.md
@@ -11,3 +11,8 @@ Using `IApplicationBuilder Use`, an extension method for adding middleware and `
 
 ## middleware 2
 Passing information from one middleware to another using `HttpContext.Items`
+
+## middleware 3
+As a general rule, only one of your Middleware should write to Response in an execution path. This sample shows how to work around this by buffering the Response.
+e.g
+If path / involves the execution of Middleware1, Middleware2 and Middleware3, only one of these should write to Response.
